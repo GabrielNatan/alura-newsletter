6d2d19929e57d2b133645ea9c4d13aeadd4e9488
gabriel natan <g.souza@grouplinknetwork.com>
2022-12-04 13:23:05 -0300

    add systemPreference

--- src/components/ToggleTheme/index.jsx
+++ src/components/ToggleTheme/index.jsx
@@ -1,8 +1,15 @@
 import { SunIcon, MoonIcon } from '@heroicons/react/24/solid'
+import { useEffect } from 'react'
 
 const ToggleTheme = () => {
+    const systemPreference = window.matchMedia('(prefers-color-scheme: dark)').matches
+    const pageClasses = document.documentElement.classList
+
+    useEffect(() => {
+        systemPreference && pageClasses.add('dark')
+    })    
     const Toggle = () => {
-        document.documentElement.classList.toggle('dark')
+        pageClasses.toggle('dark')
     }
     return <div className="hidden sm:block">
         <MoonIcon className="h-8 text-gray-100 block dark:hidden cursor-pointer" onClick={Toggle}/>
